8a76d938131402202cb55026e92309e82143d748
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-04-30 13:45:57 +0900

    Initial commit

---
+++ a001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    printf("Hello paiza\n");
+    printf("Good morning\n");
+    printf("冒険に出かけよう\n");
+    printf("世界の皆さん、こんにちは\n");
+}
+
+
+//  gcc a001.c 
+
+//  ./a.out 
